9122c36e124786e0caf6d0726a622ef4dadd4c14
Berta Lankina <berta.lankina@gmail.com>
2023-03-29 18:45:38 +0300

    Update and rename AnotherClass.java to Student.java

--- Student.java
+++ Student.java
@@ -1,14 +1,14 @@
-public class AnotherClass {
-    private int age = 32;
+public class Student {
+    private int age = 35;
     private String name = "John Doe";
 
     public int printValue() {
-        System.out.println("---" + age + "---");
+        System.out.println("----" + age + "----");
         return age;
     }
 
     public String printName() {
-        System.out.println("---" + name + "---");
+        System.out.println("----" + name + "----");
         return name;
     }
 }
